2f24a64ebfefb2461666af9e1e8e9c01705b3a46
kaylo1614 <71300529+kaylo1614@users.noreply.github.com>
2020-12-20 02:54:00 +0800

    Update project18.m

--- project18.m
+++ project18.m
@@ -1,33 +1,35 @@
-function project18(n,t)
-die=0;
-young=1;
-old=2;%待做
-last=1;
-Y=zeros(n);
+%{
+這個程式是在一個平面上，模擬單一生物的繁衍，，當成年生物附近有超過3個生物時，由於附近人太多，會導致糧食不足，使該生物死亡，周圍人數少於2人時，由於沒人可以繁衍後代，該生物會自然死去
+而對於沒有生物的的地方，如果周圍有三個成年生物，則會繁衍出生物
+使用者須輸入:n 代表你想要給多大的範圍,t 代表你想要放幾個成年人在圖上 該圖最多只有平方個點，因此，t不可大於n平方
+%}
+function project18(n,t)%n代表你想要給多大的範圍，t代表你想要放幾個成年人在圖上(t不可大於n的平方)
+die=0; %(死人) 如果周圍有3個成年人則會產生小孩
+child=1;%(活人) 剛出生的小孩
+young=2; %(活人) 代表處於可繁殖狀態
+old=3;%(活人) 代表即將自然死亡的老人
+Y=zeros(n);%紀錄圖上每個位置活死人的狀態 預設都是死人
 
-axis([0 1 0 1])
-[A ,B ,BUTTOM]=ginput(t);
-for kk=1:t
-for ii=1:n
+axis([0 1 0 1]) %先將座標畫出
+[A ,B ,BUTTOM]=ginput(t);%點了t個點
+for kk=1:t%設置成年人在滑鼠所點的那t個點的位置
+    for ii=1:n
     for jj=1:n
-        
-        if  A(kk)>(ii-1)/n && A(kk)<ii/n && B(kk)>(jj-1)/n && B(kk)<jj/n
-            Y(n-jj+1,ii)=young;
+        if  A(kk)>(ii-1)/n && A(kk)<ii/n && B(kk)>(jj-1)/n && B(kk)<jj/n %若該點x座標在ii-1跟ii之間，y座標在jj-1到jj之間則在矩陣相對應的位置紀錄
+            Y(n-jj+1,ii)=young;%因為矩陣是由左上數到右下，而座標軸是由左下數到右下，所以(ii,jj)的狀態要存在Y(n-1-jj,ii)
         end
     end
+    end
 end
-end
-image(uint8(Y));
 
 
 while last==1
-disp(Y);
-image(71*Y)
-last=0;
-X=zeros(n);
+image(uint8(Y))%映出圖形
+last=0; %初始值last=0代表族群位置已經固定了，每個族群都有固定的位置，不會畫出變化圖，last=1，代表族群人有波動，將繼續列出族群的變化，
+X=zeros(n);%初始畫周圍活人數量(預設值0代表沒有活人)
 
-
-    if Y(1,1)==young
+    %對四個角落討論:如果角落有活人，則使附近的點知道周圍有一個生物(用X來記錄，所以附近每個點的X矩陣值要加一)
+    if Y(1,1)==young 
         X(2,1)=X(2,1)+1;
         X(1,2)=X(1,2)+1;
         X(2,2)=X(2,2)+1;
@@ -48,6 +50,7 @@ X=zeros(n);
         X(n-1,n--1)=X(n-1,n-1)+1;
     end
     for ii=2:n-1
+        %對邊上做討論
         if Y(ii,1)==young
             X(ii-1,1)=X(ii-1,1)+1;
             X(ii+1,1)=X(ii+1,1)+1;
@@ -76,6 +79,7 @@ X=zeros(n);
             X(n-1,ii-1)=X(n-1,ii-1)+1;
             X(n-1,ii+1)=X(n-1,ii+1)+1;
         end
+        %對內部的點做討論:如果角落有活人，則使附近的點知道周圍有一個生物(用X來記錄，所以附近每個點的X矩陣值要加一)
         for jj=2:n-1
             if Y(ii,jj)==young
                 X(ii-1,jj-1)=X(ii-1,jj-1)+1;
@@ -89,16 +93,16 @@ X=zeros(n);
             end
         end
     end
-
+%用附近成年生物數據來改變該點狀態:如果該點有活人，則使附近的點知道周圍有一個生物(用X來記錄，所以附近每個點的X矩陣值要加一)
 for ii=1:n
     for jj=1:n
-        if Y(ii,jj)==young
+        if Y(ii,jj)==young %討論有人的狀況
             if X(ii,jj)~=2&&X(ii,jj)~=3
                 Y(ii,jj)=die;
                 last=1;
             end
         end
-        if Y(ii,jj)==die
+        if Y(ii,jj)==die %討論沒人的狀態
             if X(ii,jj)==3
                 Y(ii,jj)=young;
                 last=1;
@@ -107,4 +111,5 @@ for ii=1:n
     end
 end
 pause(1);
+end
 end
